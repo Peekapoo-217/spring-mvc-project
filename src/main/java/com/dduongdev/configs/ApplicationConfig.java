1f38cfebe990a44522542d07efbcdf03e539c843
dduongdev <nguyendongduong4551190010@gmail.com>
2025-02-19 23:36:38 +0700

    first commit

---
+++ src/main/java/com/dduongdev/configs/ApplicationConfig.java
@@ -0,0 +1,10 @@
+package com.dduongdev.configs;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.dduongdev")
+public class ApplicationConfig {
+	
+}
